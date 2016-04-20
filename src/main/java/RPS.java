a0a95b46083aaeaad22b7f3d6e9c6c14f498b40e
Blake Bless and DJ Roychoudhury <blake@>
2016-04-20 16:49:16 -0700

    aslkdf

--- src/main/java/RPS.java
+++ src/main/java/RPS.java
@@ -16,51 +16,121 @@ public class RPS {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/onePlayer", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/onePlayer.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/detector", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/detector.vtl");
 
       String input = request.queryParams("input");
+      String input2 = request.queryParams("input2");
+      RPS newGame = new RPS();
+      String winner = newGame.isRPS(input1,input2);
+
+      model.put("input", input);
+      model.put("input2", input2);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/twoPlayer", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/twoPlayer.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/detector2", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector2.vtl");
+
+      String input = request.queryParams("input");
+      String input2 = request.queryParams("input2");
       String isRPS = isRPS(input);
+      String isRPS2 = isRPS(input2);
 
       model.put("input", isRPS);
+      model.put("input2", isRPS2);
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 }
-
-
-
-
-    public static String isRPS(String input) {
-      Random cpuRandom = new Random();
-      String[] cpuSymbols = {"Rock", "Paper", "Scissors"};
-      String[] playerSymbols = {"Rock", "Paper", "Scissors"};
-      Integer num = cpuRandom.nextInt(cpuSymbols.length);
-      String cpuString = cpuSymbols[num].toString();
-      String message = "";
-        if (input.equals("Rock") && cpuString.equals("Paper")){
-          message = "cpu wins";
-        } else if (input.equals("Rock") && cpuString.equals("Scissors")){
-          message = "player wins";
-        } else if (input.equals("Paper") && cpuString.equals("Rock")){
-          message = "player wins";
-        } else if (input.equals("Paper") && cpuString.equals("Scissors")){
-          message = "cpu wins";
-        } else if (input.equals("Scissors") && cpuString.equals("Rock")){
-          message = "cpu wins";
-        } else if (input.equals("Scissors") && cpuString.equals("Paper")){
-          message = "player wins";
-        } else if (input.equals(cpuString)){
-          message = "tie";
-        }
-      //   if (symbol == playerSymbols[2]) {
-      //     return true;
-      //     .add("you tie")
-      //   } else if (symbol == "Rock" && playerSymbol == "Paper"){
-      //     return false;
-      //     .add("you lose")
-      // }
-  return message;
+  public static String Comp(){
+    Random cpuRandom = new Random();
+    String[] cpuSymbols = {"Rock", "Paper", "Scissors"};
+    Integer num = cpuRandom.nextInt(cpuSymbols.length);
+    String cpuString = cpuSymbols[num].toString();
+}
+  public static String isRPS(String input, String input2){
+    // String[] playerOneSymbols = {"Rock", "Paper", "Scissors"};
+    // String[] playerTwoSymbols = {"Rock", "Paper", "Scissors"};
+  String message = "";
+  if (input.equals("Rock") && input2.equals("Paper")){
+    message = "Player 2 wins";
+  } else if (input.equals("Rock") && input2.equals("Scissors")){
+    message = "Player 1 wins";
+  } else if (input.equals("Paper") && input2.equals("Rock")){
+    message = "Player 1 wins";
+  } else if (input.equals("Paper") && input2.equals("Scissors")){
+    message = "Player 2 wins";
+  } else if (input.equals("Scissors") && input2.equals("Rock")){
+    message = "PLayer 2 wins";
+  } else if (input.equals("Scissors") && input2.equals("Paper")){
+    message = "Player 1 wins";
+  } else if (input.equals(input2)){
+    message = "Tie";
   }
+return message;
+}
 
+
+// public static String isRPS(String input) {
+//   Random cpuRandom = new Random();
+//   String[] cpuSymbols = {"Rock", "Paper", "Scissors"};
+//   String[] playerSymbols = {"Rock", "Paper", "Scissors"};
+//   Integer num = cpuRandom.nextInt(cpuSymbols.length);
+//   String cpuString = cpuSymbols[num].toString();
+//   String message = "";
+//
+//     if (input.equals("Rock") && cpuString.equals("Paper")){
+//       message = "CPU wins";
+//     } else if (input.equals("Rock") && cpuString.equals("Scissors")){
+//       message = "Player wins";
+//     } else if (input.equals("Paper") && cpuString.equals("Rock")){
+//       message = "Player wins";
+//     } else if (input.equals("Paper") && cpuString.equals("Scissors")){
+//       message = "CPU wins";
+//     } else if (input.equals("Scissors") && cpuString.equals("Rock")){
+//       message = "CPU wins";
+//     } else if (input.equals("Scissors") && cpuString.equals("Paper")){
+//       message = "Player wins";
+//     } else if (input.equals(cpuString)){
+//       message = "Tie";
+//     }
+// return message;
+// }
+//
+//   public static String isRPS(String input, String input2){
+//     String[] playerOneSymbols = {"Rock", "Paper", "Scissors"};
+//     String[] playerTwoSymbols = {"Rock", "Paper", "Scissors"};
+//     String message = "";
+//       if (input.equals("Rock") && input2.equals("Paper")){
+//         message = "Player 2 wins";
+//       } else if (input.equals("Rock") && input2.equals("Scissors")){
+//         message = "Player 1 wins";
+//       } else if (input.equals("Paper") && input2.equals("Rock")){
+//         message = "Player 1 wins";
+//       } else if (input.equals("Paper") && input2.equals("Scissors")){
+//         message = "Player 2 wins";
+//       } else if (input.equals("Scissors") && input2.equals("Rock")){
+//         message = "PLayer 2 wins";
+//       } else if (input.equals("Scissors") && input2.equals("Paper")){
+//         message = "Player 1 wins";
+//       } else if (input.equals(input2)){
+//         message = "Tie";
+//       }
+// return message;
+// }
+//
 }
