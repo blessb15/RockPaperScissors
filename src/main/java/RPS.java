893959e4faf6d7957d3711d3c783bdae43549660
Blake Bless and DJ Roychoudhury <dj@>
2016-04-20 13:40:49 -0700

    added cpu and all RPS variations. computer picks random and test against what player picks.

--- src/main/java/RPS.java
+++ src/main/java/RPS.java
@@ -10,26 +10,59 @@ public class RPS {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/detector", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/detector.vtl");
 
+      String input = request.queryParams("input");
+      String isRPS = isRPS(input);
 
-
-
+      model.put("isRPS", isRPS);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 }
 
 
 
-    public static Boolean isRPS(String symbol) {
+    public static String isRPS(String symbol) {
       Random cpuRandom = new Random();
-      String[] cpusymbols = {"Rock", "Paper", "Scissors"};
-      Integer num = cpuRandom.nextInt(cpusymbols.length);
-      String cpuSymbol = cpusymbols[num];
-
-      if (symbol == cpusymbols[num]){
-        return true;
-
-      }else{
-        return false;
+      String[] cpuSymbols = {"Rock", "Paper", "Scissors"};
+      String[] playerSymbols = {"Rock", "Paper", "Scissors"};
+      Integer num = cpuRandom.nextInt(cpuSymbols.length);
+      String cpuString = cpuSymbols[num].toString();
+      String message = "";
+        // if (symbol == cpuSymbols[num]) {
+        //   message = "tie";
+        // } else
+        if (symbol == "Rock" && cpuString == "Paper"){
+          message = "cpu wins";
+        } else if (symbol == "Rock" && cpuString == "Scissors"){
+          message = "player wins";
+        } else if (symbol == "Paper" && cpuString == "Rock"){
+          message = "player wins";
+        } else if (symbol == "Paper" && cpuString == "Scissors"){
+          message = "cpu wins";
+        } else if (symbol == "Scissors" && cpuString == "Rock"){
+          message = "cpu wins";
+        } else if (symbol == "Scissors" && cpuString == "Paper"){
+          message = "player wins";
+        } else {
+          message = "tie";
         }
-      }
-    }
+        return message;
+
+      //   if (symbol == playerSymbols[2]) {
+      //     return true;
+      //     .add("you tie")
+      //   } else if (symbol == "Rock" && playerSymbol == "Paper"){
+      //     return false;
+      //     .add("you lose")
+      // }
+  }
+}
--- src/test/java/RPSTest.java
+++ src/test/java/RPSTest.java
@@ -1,31 +1,61 @@
-
 import java.util.ArrayList;
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class RPSTest {
+  // @Test
+  // public void isRPS_checkCpuForPaper_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Paper"));
+  // }
+  //
+  // @Test
+  // public void isRPS_checkCpuForRock_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Rock"));
+  // }
+  //
+  // @Test
+  // public void isRPS_checkCpuForScissors_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Scissors"));
+  // }
+
+  // @Test
+  // public void isRPS_checkPlayerForPaper_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Paper"));
+  // }
+  //
+  // @Test
+  // public void isRPS_checkPlayerForRock_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Rock"));
+  // }
+  //
+  // @Test
+  // public void isRPS_checkPlayerForScissors_True() {
+  //   RPS testRPS = new RPS();
+  //   Boolean expected = true;
+  //
+  //   assertEquals(expected, testRPS.isRPS("Scissors"));
+  // }
   @Test
-  public void isRPS_checkForPaper_True() {
+  public void isRPS_isMessageShowing_String(){
     RPS testRPS = new RPS();
-    Boolean expected = true;
-
-    assertEquals(expected, testRPS.isRPS("Paper"));
-  }
-
-  @Test
-  public void isRPS_checkForRock_True() {
-    RPS testRPS = new RPS();
-    Boolean expected = true;
-
+    String expected = "player wins";
     assertEquals(expected, testRPS.isRPS("Rock"));
   }
 
-  @Test
-  public void isRPS_checkForScissors_True() {
-    RPS testRPS = new RPS();
-    Boolean expected = true;
-
-    assertEquals(expected, testRPS.isRPS("Scissors"));
-  }
 
 }
